0677855c7d7ef09812e254f712f03adaa01f6752
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-09 12:59:30 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
-/** * main - Entry Point "Programming is like building a multilingual puzzle" * Return: Always 0 (Success) */ int main(void) { 
+
+/**
+ * main - Entry Point "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void) {
+	 
 	puts("\"Programming is like building a multilingual puzzle"); return (0);
 }
