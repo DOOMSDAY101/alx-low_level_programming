8d8d747a8a6da340fd9c2b37e55e63a273092921
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-09 13:35:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * main - Entry Point "Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
- 
-int main(void) {
-	 
-	puts("\"Programming is like building a multilingual puzzle"); return (0);
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
