2932181e09512cc7c7ea8a44f97d63b08e5f0621
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-09 13:12:46 -0700

    fixed printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * main - Entry point "with proper grammar, but the outcome is a piece of art,
+ * Return: always 0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
