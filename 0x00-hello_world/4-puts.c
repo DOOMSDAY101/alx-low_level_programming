1bafb9696057e5956636b79aacdd78dbe9bfdbbb
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-09 12:53:04 -0700

    fixed puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
-}
+
+
+ puts("\"Programming is like building a multilingual puzzle\n"); 
+return (0); 
+} 
