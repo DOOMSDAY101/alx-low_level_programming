874b81d25f4f0f4d9cf1b4c6a0d70be523827c94
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-12 07:25:32 -0700

    just numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always (0) (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+	putchar('\n');
+	return (0);
+}
