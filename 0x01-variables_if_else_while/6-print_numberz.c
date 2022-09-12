5352b833e7a721ea37fd18d59621d91100a46f6c
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-12 07:36:38 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always (0) (success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+{
+	putchar(i);
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
