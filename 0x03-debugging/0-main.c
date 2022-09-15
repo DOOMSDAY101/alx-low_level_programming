0d1086aca7608f1b45d11791b9659eedf9ce3c98
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-15 14:11:54 -0700

    fixed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
  * main - Entry
  * Return: 0
  **/
 int main(void)
-{
-	int i = 0;
 
-	positive_or_negative(i);
-	return (0);
+{
+
+int i = 0;
+
+positive_or_negative(i);
+
+return (0);
 }
