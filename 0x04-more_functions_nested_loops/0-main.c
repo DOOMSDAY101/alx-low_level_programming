6e899c68bc6c6a6ec28542fb49b39259f79927d4
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-15 10:44:06 -0700

    0-main.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,20 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
-/**
- * _isupper - check the code.
- * @c : number to be checked
- * Return: Always 0.
- */
-int _isupper(int c)
-{
-	char c;
-
-	c = 'A';
-
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+/** * main - check the code. * * Return: Always 0. */ int main(void) { char c; c = 'A'; printf("%c: %d\n", c, _isupper(c)); c 
+    = 'a'; printf("%c: %d\n", c, _isupper(c)); return (0);
 }
