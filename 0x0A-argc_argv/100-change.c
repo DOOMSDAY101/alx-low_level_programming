d4f7e9b6e8811d8509ed410458b117a631568acb
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-09-30 01:44:58 -0700

    task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int change(int cents);
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("%s\n", "Error");
+return (1);
+}
+else if (argc < 0)
+{
+return (0);
+}
+printf("%d\n", change(atoi(argv[1])));
+return (0);
+}
