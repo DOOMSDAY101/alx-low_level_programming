a00c89ea6817533f37b8de95e8977e12572a3150
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-10-28 03:33:30 -0700

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
-#include <stdio.h>
-ifdef DEBUG /*prints debug statements*/
-endif
+/*
+ * File: main.h
+ * AUthor: IFEOLUWA
+ * Desc: Header file containing prototypes for all functions
+ * written in the 0x14-bit_manipulation directory.
+ */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
