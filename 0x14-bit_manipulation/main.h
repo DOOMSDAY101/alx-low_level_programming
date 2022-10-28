e61e5e1712a2337af14e687566093d4a5c911971
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-10-28 03:18:41 -0700

    main.h fixed

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- * File: main.h
- * AUthor:Ifeoluwa
- * Desc: Header file containing prototypes for all functions
- * written in the 0x14-bit_manipulation directory.
- */
+#include <stddef.h>
+#include <stdio.h>
+ifdef DEBUG /*prints debug statements*/
+endif
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
