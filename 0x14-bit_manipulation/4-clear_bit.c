4c0dc3f70e5926f670a784d748a4c555533b1686
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-10-28 03:54:56 -0700

    task 4 completed

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,19 @@
+/*
+ * File: 4-clear_bit.c
+ */
 #include "main.h"
 /**
- * clear_bit - Entry Point
- * @n: input
- * @index: index
- * Return: 0
+ * clear_bit - Sets the value of a bit at a given index to 0.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ * Otherwise - 1.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int bits;
-if (n == NULL)
+if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
-bits = 1 << index;
-if ((bits | *n) == *n)
-*n = *n ^ bits;
+*n &= ~(1 << index);
 return (1);
 }
