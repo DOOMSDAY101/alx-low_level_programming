38abd5ea6f865145f3d4e6507bbf7857d3277391
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2022-10-28 03:43:31 -0700

    task 1 completed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,14 @@
+/*
+ * File: 1-print_binary.c
+ */
 #include "main.h"
 /**
- * print_binary - Entry Point
- * @n: dec input
- * Return: 0
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
-int i = 0, count, k, temp;
-if (n == 0)
-{
-printf("0");
-return;
-}
-temp = n;
-while (temp != 0)
-{
-i++;
-temp = temp >> 1;
-}
-for (count = i - 1; count >= 0; count--)
-{
-k = n >> count;
-if (k & 1)
-printf("1");
-else
-printf("0");
-}
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
